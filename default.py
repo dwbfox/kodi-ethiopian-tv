262ca60de5672eaac38223643cc9953b02be036c
dwbfox <dwb.misc@gmail.com>
2016-04-27 16:33:24 -0500

    Added files to repo, added README and other initial commits

---
+++ default.py
@@ -0,0 +1,42 @@
+﻿import xbmcaddon
+import xbmcgui
+import xbmcplugin
+import sys
+import re
+import urllib2
+
+stream_sources = {
+    'EBS TV': 'http://www.vixtream.net/vcdn/playerf.php?chan=ebstv',
+    'EBC 1': 'http://www.vixtream.net/vcdn/ebctv.php',
+    'EBC 2': 'http://www.vixtream.net/vcdn/ebctv.php?chan=ebc2',
+    'EBC 3': 'http://www.vixtream.net/vcdn/ebctv.php?chan=ebc3'
+}
+
+
+def get_stream_urls():
+    """Retrieves actual streamable URLs from the sources provided"""
+    streams = {}
+    for stream_source in stream_sources:
+        source = urllib2.urlopen(stream_sources[stream_source]).read()
+        try:
+            regex = re.compile('file:\s*"(.+)"')
+            streams[stream_source] = regex.search(source).group(1)
+        except:
+            streams[stream_source + '(Unavailable)'] = None
+    return streams
+
+
+def main():
+    proc_handle = int(sys.argv[1]);
+    stream_urls = get_stream_urls()
+
+    for stream_url in stream_urls:
+
+        # Show the listing
+        listItem = xbmcgui.ListItem(stream_url)
+        listItem.setIconImage('icon.png')
+        xbmcplugin.addDirectoryItem(handle=proc_handle, url=stream_urls[stream_url], listitem=listItem)
+
+    xbmcplugin.endOfDirectory(proc_handle)
+
+main()
